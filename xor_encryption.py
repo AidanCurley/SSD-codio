a40a567bf1d323ffecc38093b5bc31d1a10c24a1
AidanCurley <aidanpcurley@gmail.com>
2022-04-28 16:40:26 +0100

    Refactore method names

--- xor_encryption.py
+++ xor_encryption.py
@@ -5,16 +5,16 @@ def generate_key(length: int) -> bytes:
     return urandom(length)
 
 
-def xor_strings(s, t) -> bytes:
+def xor(s, t) -> bytes:
     return bytes([a ^ b for a, b in zip(s, t)])
 
 
 def encrypt(text, key):
-    return xor_strings(text.encode('utf-8'), key)
+    return xor(text.encode('utf-8'), key)
 
 
 def decrypt(text, key):
-    return xor_strings(text, key).decode('utf-8')
+    return xor(text, key).decode('utf-8')
 
 
 message = input('Type in your message: ')
