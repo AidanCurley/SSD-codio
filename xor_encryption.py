602307486917841b2f4e176088899a5edc044a22
AidanCurley <aidanpcurley@gmail.com>
2022-04-28 11:43:32 +0100

    Update xor_encryption

--- xor_encryption.py
+++ xor_encryption.py
@@ -1,20 +1,31 @@
-def xor_crypt_string(data, key='awesomepassword', encode=False, decode=False):
-    from itertools import cycle
-    import base64
-
-    if decode:
-        data = data.decode()
-    xored = ''.join(chr(ord(x) ^ ord(y)) for (x, y) in zip(data, cycle(key)))
-
-    if encode:
-        s = xored.encode()
-        return base64.encodebytes(s).strip()
-    return xored
+from os import urandom
 
 
-secret_data = "XOR procedure"
+def generate_key(length: int) -> bytes:
+    return urandom(length)
 
-print("The cipher text is")
-print(xor_crypt_string(secret_data, encode=True))
-print("The plain text fetched")
-print(xor_crypt_string(xor_crypt_string(secret_data, encode=True), decode=True))
+
+def xor_strings(s, t) -> bytes:
+    return bytes([a ^ b for a, b in zip(s, t)])
+
+
+def encrypt(text, key):
+    return xor_strings(text.encode('utf-8'), key)
+
+
+def decrypt(text, key):
+    return xor_strings(text, key).decode('utf-8')
+
+
+message = input('Type in your message: ')
+
+key = generate_key(len(message))
+print('Key: ', key)
+
+cipherText = encrypt(message, key)
+print('cipherText: ', cipherText)
+
+
+print('decrypted: ', decrypt(cipherText, key))
+
+
